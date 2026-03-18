a4ff005c9dc1cda2d596513fd338c07f3b207729
Dan5567889 <dantehernandez587@gmail.com>
2026-03-18 15:00:57 -0400

    day 5

--- settings.js
+++ settings.js
@@ -1,23 +1,30 @@
 const toggleDarkMode = () => {
     const body = document.body;
+    
     const nav = document.querySelector("nav");
+    const container = document.querySelector(".container");
+    const loginContainer = document.querySelector(".login-container");
     body.classList.toggle("dark-mode");
     nav.classList.toggle("dark-mode");
+    container.classList.toggle("dark-mode");
+    loginContainer.classList.toggle("dark-mode");
     console.log("Dark mode toggled");
 
-if (body.classList.contains("dark-mode") && nav.classList.contains("dark-mode")) {
+if (body.classList.contains("dark-mode") && nav.classList.contains("dark-mode") && container.classList.contains("dark-mode") && loginContainer.classList.contains("dark-mode")) {
         localStorage.setItem("theme", "dark");
     } else {
         localStorage.setItem("theme", "light");
     }
 };
 
-
-
 document.addEventListener("DOMContentLoaded", (event) => {
     if (localStorage.getItem("theme") === "dark") {
         document.body.classList.add("dark-mode");
         const nav = document.querySelector("nav");
         nav.classList.add("dark-mode");
+        const container = document.querySelector(".container");
+        container.classList.add("dark-mode");
+        const loginContainer = document.querySelector(".login-container");
+        loginContainer.classList.add("dark-mode");
     }
 });
